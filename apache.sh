63da69ed6affef989f0bfffd1c223baf843bf25e
valiant158 <lijiafeng.123456@gmail.com>
2018-05-24 21:11:32 +0800

    Create apache.sh

---
+++ apache.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install apache2
+sudo apt-get install php
+sudo apt-get install libapache2-mod-php7.0
+// 配置APACHE+PHP7的
+sudo apt-get install libapache2-mod-php
+// 这个应该是配置APACHE+PHP5的，一块装吧
+apt-get install mysql-server mysql-client
